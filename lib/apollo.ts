af7599f9086a16f44b65b8898e3542767408677b
Cesar Landeros <me@cesarlanderos.dev>
2022-06-13 18:30:05 -0500

    Add fragment types and postgres docker container

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,8 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import introspectionQueryResultData from '../graphql/__generated__/fragmentTypes.json';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    possibleTypes: introspectionQueryResultData.possibleTypes,
+  }),
 });
 
 export default client;
