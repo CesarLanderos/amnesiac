9c24596dd822f99f7c009d2ab3a6cb803a0b4022
Cesar Landeros <me@cesarlanderos.dev>
2022-06-14 01:18:04 -0500

    set spotify correctly

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,27 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <script async src="https://sdk.scdn.co/spotify-player.js" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
+        <script async src="https://sdk.scdn.co/spotify-player.js" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.onSpotifyWebPlaybackSDKReady = () => {
                 const token =
-                  'BQBILGa_r9eFDQJQrS4lb7RKOfRmboofbBVhkN2PMjEEcPwKaESVUtawssUd_R9fj9V_kWkOyCthYrMZHWol3Q0U6ZWd0LKAbzWCXyRxfxYH_whF8Wt_fs1UGun5q92HSoZMTOuXqGvASFF9nYeLHFY1Wo6GjZW9Pw76LmicD2bFcxZRKxlbSqTNFzVl4a_fozoAeWo';
+                  'BQCnzkULBm5Tgjqkfla9IDp-C5CkaR-xLC6EwCWz0v1IwBIQDss8qkNCus0mg8x5eRar93F__yqSj6B3t3gozXKLjt7nXeoKo2wDkeR-EoGTdwxz_nPjojQdWklhDkZcbO4iorWiRyl5QboKNGFLySMeY97m7R08ARw62kqoAKjkqHrfY0Mp5zOR3dANBARkhe-F6BI';
                 const player = new Spotify.Player({
                   name: 'Web Playback SDK Quick Start Player',
                   getOAuthToken: (cb) => {
+                    console.log('herewego')
                     cb(token);
                   },
                   volume: 0.5,
                 });
-
-                console.log('heeeer');
 
                 // Ready
                 player.addListener('ready', ({ device_id }) => {
@@ -36,15 +34,15 @@ export default function Document() {
                 });
 
                 player.addListener('initialization_error', ({ message }) => {
-                  console.error(message);
+                  console.error('initialization_error', message);
                 });
 
                 player.addListener('authentication_error', ({ message }) => {
-                  console.error(message);
+                  console.error('authentication_error', message);
                 });
 
                 player.addListener('account_error', ({ message }) => {
-                  console.error(message);
+                  console.error('account_error',message);
                 });
 
                 player.connect();
